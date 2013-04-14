1ed5f465fc952253fcddbbc280fa42b4c49e766b
crynobone <crynobone@gmail.com>
2013-04-14 19:25:22 +0800

    Add Orchestra\Extension\ConfigManager
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Extension/ConfigManager.php
@@ -0,0 +1,47 @@
+<?php namespace Orchestra\Extension;
+
+class ConfigManager {
+	
+	/**
+	 * Application instance.
+	 *
+	 * @var Illuminate\Foundation\Application
+	 */
+	protected $app = null;
+
+	/**
+	 * Construct a new Orchestra\Extension\ConfigManager instance.
+	 *
+	 * @access public
+	 * @param  Illuminate\Foundation\Application    $app
+	 * @return void
+	 */
+	public function __construct($app)
+	{
+		$this->app = $app;
+	}
+
+	/**
+	 * Map configuration to allow orchestra to store it in database
+	 *
+	 * @access public
+	 * @param  string   $name
+	 * @param  array    $maps
+	 * @return void
+	 */
+	public function map($name, $maps)
+	{
+		$config = $this->app['config'];
+		$memory = $this->app['orchestra.memory']->make();
+		$meta   = $memory->get("extension_{$name}", array());
+
+		foreach ($maps as $current => $default)
+		{
+			isset($meta[$current]) and $config->set($default, $meta[$current]);
+
+			$meta[$current] = $config->get($default);
+		}
+
+		$memory->put("extension_{$name}", $meta);
+	}
+}
---
+++ tests/ConfigManagerTest.php
@@ -0,0 +1,52 @@
+<?php namespace Orchestra\Extension\Tests;
+
+class ConfigManagerTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		\Mockery::close();
+	}
+
+	/**
+	 * Test Orchestra\Extension\ConfigManager::map() method.
+	 *
+	 * @test
+	 */
+	public function testMapMethod()
+	{
+		$app = array(
+			'orchestra.memory' => ($memory = \Mockery::mock('\Orchestra\Memory\MemoryManager')),
+			'config' => ($config = \Mockery::mock('Config')),
+		);
+
+		$memory->shouldReceive('make')
+				->once()->andReturn($memory)
+			->shouldReceive('get')
+				->once()->with('extension_laravel/framework', array())
+				->andReturn(array('foobar' => 'foobar is awesome'))
+			->shouldReceive('put')
+				->once()
+				->with('extension_laravel/framework', array('foobar' => 'foobar is awesome', 'foo' => 'foobar'))
+				->andReturn(true);
+
+		$config->shouldReceive('set')
+				->once()->with('laravel/framework::foobar', 'foobar is awesome')
+				->andReturn(true)
+			->shouldReceive('get')
+				->once()->with('laravel/framework::foobar')
+				->andReturn('foobar is awesome')
+			->shouldReceive('get')
+				->once()->with('laravel/framework::foo')
+				->andReturn('foobar');
+
+		$stub = new \Orchestra\Extension\ConfigManager($app);
+
+		$stub->map('laravel/framework', array(
+			'foo'    => 'laravel/framework::foo',
+			'foobar' => 'laravel/framework::foobar',
+		));
+	}
+}

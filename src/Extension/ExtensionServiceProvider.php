e0e4221a41233a934f033b957713f7f5d9d46590
crynobone <crynobone@gmail.com>
2014-03-03 11:32:21 +0800

    Move to PSR-4.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Extension/ExtensionServiceProvider.php
+++ src/Extension/ExtensionServiceProvider.php
@@ -75,7 +75,7 @@ class ExtensionServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $path = realpath(__DIR__.'/../../');
+        $path = realpath(__DIR__.'/../');
 
         $this->package('orchestra/extension', 'orchestra/extension', $path);
     }

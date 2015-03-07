021634969731e90ae8fe5136dbcd14a59eece900
crynobone <crynobone@gmail.com>
2015-03-07 20:09:02 +0800

    Remove unused code.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Console/BaseCommand.php
+++ src/Console/BaseCommand.php
@@ -1,8 +1,6 @@
 <?php namespace Orchestra\Extension\Console;
 
 use Illuminate\Console\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class BaseCommand extends Command
 {

bf3f5106166f302d87dc663e6592f5c6cc5ee0a2
crynobone <crynobone@gmail.com>
2015-03-07 20:07:37 +0800

    Remove requirement to explicitly finish/terminate orchestra/memory during console command.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Console/BaseCommand.php
+++ src/Console/BaseCommand.php
@@ -6,29 +6,5 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class BaseCommand extends Command
 {
-    /**
-     * Execute the console command.
-     *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
-     *
-     * @return void
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        parent::execute($input, $output);
-
-        $this->finish();
-    }
-
-    /**
-     * Finish the console command.
-     *
-     * @return void
-     */
-    protected function finish()
-    {
-        // Save any changes to orchestra/memory
-        $this->laravel['orchestra.memory']->finish();
-    }
+    //
 }
